88a93a184a40809802a5ab80b25d313d4af11971
Rohit-del-source <rohitverma18158920@gmail.com>
2024-01-15 12:08:32 +0530

    done

--- components/UsersList.js
+++ components/UsersList.js
@@ -1,11 +1,12 @@
 import { useMemo } from 'react';
 
 function UsersList({ users, startingLetter }) {
-  
-
+  const filteredUsers =useMemo( ()=>{
+    return users.filter(user=>user.name.startsWith(startingLetter))
+  },[startingLetter])
   return (
     <ul>
-      <li key={}>{}</li>
+      {filteredUsers.map(item => <li key={item.id}>{item.name}</li>)}
     </ul>
   );
 }
